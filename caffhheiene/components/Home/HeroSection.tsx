1c0941d5c7f236a873eb1f38c7fc9de998d92896
SWARVY <swarvy0826@naver.com>
2024-02-05 16:06:32 +0900

    feat : HeroSection에 설명문구 추가

--- caffhheiene/components/Home/HeroSection.tsx
+++ caffhheiene/components/Home/HeroSection.tsx
@@ -1,3 +1,15 @@
+import INFO from '@/constants/Info'
+
 export default function HeroSection() {
-  return <div className="grid h-screen" />
+  return (
+    <div className="grid h-full justify-items-center items-center text-white font-bold">
+      <ul className="text-4xl space-y-3 lg:text-5xl ">
+        {INFO.descriptions.map((description) => (
+          <li>
+            <p>{description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
