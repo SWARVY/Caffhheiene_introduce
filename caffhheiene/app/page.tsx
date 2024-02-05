4da96525a888f09fb816850a243703a6eba58aed
SWARVY <swarvy0826@naver.com>
2024-02-05 16:07:33 +0900

    feat : 메인 페이지에서 생성된 컴포넌트 로드

--- caffhheiene/app/page.tsx
+++ caffhheiene/app/page.tsx
@@ -1,19 +1,31 @@
 import HeroSection from '@/components/Home/HeroSection'
 import SideNaviagtor from '@/components/Home/SideNavigator'
+import InfoContainer from '@/components/Info/InfoContainer'
+import ProjectContainer from '@/components/Project/ProjectContainer'
+import SidebarToc from '@/components/SidebarToc'
 import SETTINGS from '@/constants/Settings'
 
 export default function Home() {
   return (
-    <div className="flex-col font-Pretendard_Rg">
-      <div className="grid grid-cols-3">
+    <div className="grid gap-y-10 font-Pretendard_Rg">
+      <div className="grid lg:grid-cols-4">
         <SideNaviagtor />
         <div
-          className="col-span-2 bg-contain bg-right bg-fixed bg-no-repeat brightness-75"
+          className="bg-cover bg-bottom bg-local bg-no-repeat backdrop-brightness-75 md:bg-fixed lg:col-span-3 "
           style={{
             backgroundImage: `url(${SETTINGS.mainBackgroundImage})`,
           }}
         >
           <HeroSection />
+        </div>
+      </div>
+      <div className="grid lg:grid-cols-4">
+        <div className="invisible lg:visible">
+          <SidebarToc />
+        </div>
+        <div className="grid gap-y-10 lg:col-span-3">
+          <InfoContainer />
+          <ProjectContainer />
         </div>
       </div>
     </div>
